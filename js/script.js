4e71c8b2f1086e2417ad4760eee6a53527f8494e
Ansh <25cs001@charusat.edu.in>
2026-07-15 21:37:42 +0530

    Practical-1

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// StudentHub - script.js
+// Beginner-level JS: highlights the current page's link in the navbar
+
+window.onload = function () {
+  var links = document.querySelectorAll("nav ul li a");
+  var currentPage = window.location.pathname.split("/").pop();
+
+  for (var i = 0; i < links.length; i++) {
+    var linkPage = links[i].getAttribute("href");
+    if (linkPage === currentPage) {
+      links[i].style.fontWeight = "bold";
+      links[i].style.textDecoration = "underline";
+    }
+  }
+};
